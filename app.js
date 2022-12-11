ae043ed51c6080d091b86b18a3ca6a8496862a69
itsmeganesh <itsmeganeshadhikari@gmail.com>
2022-12-11 06:32:52 +0545

    BMC-mongo-0.0 First commit to mongodb

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv').config()
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
+const connection = require('./src/db/connection');
 const debug = require('debug')('app')
 
 
@@ -8,6 +9,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
+connection()
 
 app.get('/', () => console.log(`Main page`))
 app.get('/home', (req, res) => res.send('Hello to home page'))
