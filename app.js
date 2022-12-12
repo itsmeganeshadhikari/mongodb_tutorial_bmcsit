edf7736b99511d3e5017d7303f741fd0006b8188
sandesh <sandeshingionee@gmail.com>
2022-12-12 10:02:30 +0545

    just commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv').config()
-const cookieParser = require('cookie-parser')
-const dbConnection = require('./src/db/connection');
+const cookieParser = require('cookie-parser');
+const connection = require('./src/db/connection');
 const debug = require('debug')('app')
 
 
@@ -10,6 +10,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 dbConnection()
+
 app.get('/', () => console.log(`Main page`))
 app.get('/home', (req, res) => res.send('Hello to home page'))
 
