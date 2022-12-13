f3783d19fd67fd018c060cb888abb6a91d18d7d7
itsmeganesh <itsmeganeshadhikari@gmail.com>
2022-12-13 13:52:40 +0545

    BMC-mongodb-0.0 "Add async and await call"

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,10 +1,10 @@
 const User = require("../models/User")
 
-exports.create = (req, res) =>{
+exports.create = async(req, res) =>{
     console.log(req.body);
     try {
         const newUser =  new User(req.body)
-        newUser.save()
+        await newUser.save()
         res.send({user:newUser})
     } catch (error) {
         res.status(500).send({message:"Error"})
